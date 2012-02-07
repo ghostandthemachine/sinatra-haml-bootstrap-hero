3fb0d024be081c2ef9c7fbc3a7e5251317c50db3
Jon <jonrose08@gmail.com>
2012-02-07 07:14:31 -0800

    renew

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+set :application, "ghostandthemachines.com/sinatra-haml-bootstrap-basic-hero"
+set :user,        "jon"
+set :repository, "git@ghostandthemachines.com:sinatra-haml-bootstrap-hero.git"
+set :domain, "ghostandthemachines.com"
+set :deploy_to, "~/sites/#{application}"
+=======
+set :application, 	"yoursite.com/sinatra-haml-bootstrap-basic-hero"
+set :user,        	"user"
+set :repository, 	"git@yourrepo.com:sinatra-haml-bootstrap-hero.git"
+set :domain, 		"yoursite.com"
+set :deploy_to, 	"~/sites/#{application}"
+>>>>>>> 3f58f8fc8a391cdcfbeb6218cb465345bc19fca6
